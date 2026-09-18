58ebe4e5f3f151c4d7ea62375ca1750742e30760
Tejas-Anil-Kulkarni <kulkarnitejas.dev@gmail.com>
2026-09-18 15:27:06 +0530

    Data transformation

--- main.py
+++ main.py
@@ -32,12 +32,12 @@ if __name__=='__main__':
 
         print(data_validation_artifact)
         
-        # data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
-        # logging.info("data Transformation started")
-        # data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
-        # data_transformation_artifact=data_transformation.initiate_data_transformation()
-        # print(data_transformation_artifact)
-        # logging.info("data Transformation completed")
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
 
         # logging.info("Model Training sstared")
         # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
